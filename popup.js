25df2d0cb401447a40709c2304fb3429d25eb730
Steven Xu <SXu@prysm.com>
2017-06-28 12:35:18 -0700

    Implemented autorefreshing of the current page

--- popup.js
+++ popup.js
@@ -1,3 +1,27 @@
 "use strict";
 
-document.getElementById("classRefresh").onclick = function() {  alert('hello!'); };
+var refreshClock = {
+  onHandler : function(e) {
+    chrome.alarms.create("refreshAlarm", {
+      delayInMinutes: 0,
+      periodInMinutes: 0.1
+    } );
+    window.close();
+  },
+
+  offHandler : function(e) {
+    chrome.alarms.clear("refreshAlarm");
+    window.close();
+  },
+
+  setup: function() {
+    var e = document.getElementById('refreshOn');
+    e.addEventListener('click',  refreshClock.onHandler );
+    var e = document.getElementById('refreshOff');
+    e.addEventListener('click',  refreshClock.offHandler );
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    refreshClock.setup();
+});
