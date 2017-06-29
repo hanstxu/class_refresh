2a2fba77cc3897933cfe4162dab5e08bc1935f16
Steven Xu <hanstxu@gmail.com>
2017-06-29 12:01:13 -0700

    manifest.json, eventPage.js: cleaned up spacing and extraneous code

--- eventPage.js
+++ eventPage.js
@@ -1,8 +1,3 @@
-/*chrome.alarms.onAlarm.addListener(function(alarm) {
-  chrome.tabs.reload();
-});
-*/
-
 chrome.alarms.onAlarm.addListener(function(alarm) {
   chrome.storage.local.get('TabID', function (result) {
     chrome.tabs.reload(result.TabID);
