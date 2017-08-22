d24d478699769d792c75b86c9caa4b46598563dc
Steven Xu <hanstxu@gmail.com>
2017-08-21 17:01:34 -0700

    Finally finished implementing the extension; checked if a specific field on the class webpage contained Open; made sure the content script only works on UCLA class pages; used the Chrome notifications API to notify the user if a class is open

--- content.js
+++ content.js
@@ -1,6 +1,10 @@
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.text === 'report_back') {
-    sendResponse(document.all[0].outerHTML);
+    var enrl_mtng_info = document.getElementById("enrl_mtng_info");
+    var info_table = enrl_mtng_info.firstElementChild.nextElementSibling;
+    var enrollment_status = info_table.firstElementChild.firstElementChild;
+    var check_string = enrollment_status.innerHTML.substring(0,4);
+    sendResponse((check_string == "Open"));
   }
 });
--- eventPage.js
+++ eventPage.js
@@ -1,13 +1,27 @@
-chrome.alarms.onAlarm.addListener(function(alarm) {
-  chrome.storage.local.get('TabID', function (result) {
-    //chrome.tabs.reload(result.TabID);
-  });
-});
-
-function doStuffWithDom(domContent) {
-  console.log('DOM content:\n' + domContent);
+function notify(matched) {
+  if (matched) {
+    chrome.notifications.create(
+      'open_class',
+      {
+        type: 'basic',
+        iconUrl: 'icon128.png',
+        title: "Class is now open!",
+        message: "Hurry up and sign up!"
+      },
+      function() {}
+    );
+  }
+  else {
+    chrome.notifications.clear(
+      'open_class',
+      function() {}
+    );
+  }
 }
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  chrome.storage.local.get('TabID', function (result) {
+    chrome.tabs.reload(result.TabID);
+    chrome.tabs.sendMessage(result.TabID, {text: 'report_back'}, notify);
+  });
 });
